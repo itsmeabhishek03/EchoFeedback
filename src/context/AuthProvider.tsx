6a551ce00e192f7c6b97aae3d2fa2c9bfb133eb6
Abhishek Mishra <abhimishraop08@gmail.com>
2024-11-03 19:07:43 +0530

    first commit

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,34 @@
+// 'use client';
+
+// import { SessionProvider } from 'next-auth/react';
+
+// export default function AuthProvider({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <SessionProvider>
+//       {children}
+//     </SessionProvider>
+//   );
+// }
+
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  );
+}
+
+
+
